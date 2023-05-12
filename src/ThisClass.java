5e7565bb09794b05cf19067f2cede8bd743dae73
DevikaRajesh22 <devikaraj699@gmail.com>
2023-05-12 11:22:16 +0530

    this keyword

---
+++ src/ThisClass.java
@@ -0,0 +1,9 @@
+public class ThisClass {
+    int a=100,b=200;
+    ThisClass(int a,int b){
+        this.a=a;
+        this.b=b;
+        System.out.println("this.a = "+a+" this.b = "+b);
+
+    }
+}
---
+++ src/ThisMain.java
@@ -0,0 +1,5 @@
+public class ThisMain {
+    public static void main(String[] args){
+        ThisClass obj=new ThisClass(10,20);
+    }
+}
